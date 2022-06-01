1eacdc508d6e73a0ef222003ea8524506665f40e
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2022-05-31 23:15:11 -0400

    Interfaz registro pedido modificada y conectada con BD

--- resources/views/emideli/registrar_pedido.blade.php
+++ resources/views/emideli/registrar_pedido.blade.php
@@ -50,86 +50,88 @@ Agregar Pedido
                 This is some text within a card body.
             </div>
         </div>
-        <div class="card-body">
-            <form action="{{ route('prueba')}}" method="POST">
-                
-                @csrf
 
-                <div class="mb-3">
-                    <label for="tipo_pedido" class="form-label">Tipo de Pedido</label>
-                    <input type="text" name="tipo_pedido" id="tipo_pedido" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3">
-                    <label for="nombre_cliente" class="form-label">Nombre del Cliente</label>
-                    <input type="text" name="nombre_cliente" id="nombre_cliente" class="form-control" id="exampleInputPassword1">
-                </div>
-                
-                <div class="mb-3">
-                    <label for="abono" class="form-label">Abono</label>
-                    <input type="number" name="abono" id="abono" class="form-control" id="exampleInputPassword1">
+        <div class="col card m-1" style="max-width: 300px;">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <!-- Modal -->
+    <div class="modal fade modal-dialog-scrollable" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content" style="background-color: #FFBBBB;">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agendar pedido</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form method='post'>
+                    <form action="{{ route('prueba')}}" method="POST">
                         @csrf
+
+                        <!-- //TODO Agregar tipo pedido aqui -->
+                        <div class="mb-3">
+                            <label for="contacto" class="form-label">Tipo pedido</label>
+                            <input type="text" class="form-control" id="contacto" name="tipo_pedido">
+                        </div>
+                        
+                        <!-- //*Nombre cliente -->
                         <div class="mb-3">
                             <label for="contacto" class="form-label">Cliente</label>
                             <div class="input-group mb-3">
                                 <div class="input-group-text">
                                     <input class="form-check-input mt-0" type="checkbox" value="" id="type_field">
                                 </div>
-                                <input type="text" class="form-control" name="nombre" id="name_client_txt">
+                                <input type="text" class="form-control" name="nombre_cliente" id="name_client_txt">
                             </div>
-                            <select class="form-select" name="nombre" id="name_client_dropbox">
+                            <select class="form-select" name="nombre_cliente" id="name_client_dropbox">
                                 <option value="invalid">--Selecciones una opcion--</option>
-                                //*insetar option desde la base de datos
+                                //TODO insetar option desde la base de datos
                             </select>
                         </div>
+
+                        <!-- //*Abono pedido -->
                         <div class="mb-3">
                             <label for="contacto" class="form-label">Abono</label>
-                            <input type="text" class="form-control" id="contacto" name="contacto">
+                            <input type="number" class="form-control" id="contacto" name="abono">
                         </div>
+
+                        <!-- //*Fecha de entrega -->
                         <div class="mb-3">
                             <label for="direccion" class="form-label">Fecha de entrega</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion">
+                            <input type="date" class="form-control" id="direccion" name="fecha_entrega">
                         </div>
-                        <div class="mb-3">
-                            <label for="direccion" class="form-label">Fecha de creacion</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion">
-                        </div>
+
+                        <!-- //*Imagen -->
                         <div class="mb-3">
                             <label for="direccion" class="form-label">Imagen</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion">
+                            <input type="text" class="form-control" id="direccion" name="imagen">
                         </div>
+
+                        <!-- //*Total -->
                         <div class="mb-3">
                             <label for="direccion" class="form-label">Total</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion">
+                            <input type="number" class="form-control" id="direccion" name="total_pedido">
                         </div>
+
+                        <!-- //*Descripcion -->
                         <div class="mb-3">
                             <label for="direccion" class="form-label">Descripcion</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion">
+                            <textarea type="text" class="form-control" id="direccion" name="descripcion"></textarea>
                         </div>
+
                         <button type="submit" style="background-color: #97DBAE; border: none; box-shadow: 2px 2px 5px #999;" class="btn btn-success">Guardar</button>
                     </form>
                 </div>
-<<<<<<< HEAD
-                <div class="mb-3">
-                    <label for="imagen" class="form-label">Imagen referencial</label>
-                    <input type="text" name="imagen" id="imagen" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="total_pedido" class="form-label">Total pedido</label>
-                    <input type="number" name="total_pedido" id="total_pedido" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3">
-                    <label for="descripcion" class="form-label">Descripcion</label>
-                    <textarea class="form-control" name="descripcion" id="descripcion" rows="3"></textarea>
-                </div>
-                
-                <button type="submit" class="btn btn-primary">Agendar</button>
-            </form>
-=======
             </div>
->>>>>>> 78d7615b798dbbed0e29f867db31621f33ae2d4b
         </div>
     </div>
 </div>
