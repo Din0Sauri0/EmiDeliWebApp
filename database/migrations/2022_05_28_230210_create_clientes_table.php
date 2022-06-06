0dafb17ffa9927599aa2bb909964506f4a9e2870
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2022-06-05 22:10:39 -0400

    login con la app movil funcionando

--- database/migrations/2022_05_28_230210_create_clientes_table.php
+++ database/migrations/2022_05_28_230210_create_clientes_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('contacto', 50);
             $table->string('direccion')->nullable();
 
-            $table->unsignedBigInteger('user_id')->unique();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
 
             $table->timestamps();
