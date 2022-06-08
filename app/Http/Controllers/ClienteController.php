1d8450e50def2af9f61cd483b5eef344dd88140e
CamilaCA99 <70529959+CamilaCA99@users.noreply.github.com>
2022-06-08 15:18:58 -0400

    crear cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,23 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 
 class ClienteController extends Controller
 {
     public function index(){
-        return view ('emideli.registrar_cliente');
+
+        $clients= Cliente::all();
+        return view ('emideli.registrar_cliente', compact('clients'));
     }
-    public function create(Request $request){
+    public function create(Request $request ){
         $cliente = new Cliente();
 
         $cliente->nombre = $request->nombre;
         $cliente->contacto = $request->contacto;
         $cliente->direccion = $request->direccion;
-
-        /*FALTA LA LA LLAVE FORANEA USER*/
+        $cliente->user_id= auth()->id();
 
         $cliente->save();
+
+        return redirect('/cliente');
     }
+
 }
